e02733808464f77cade92a264278782a80ea7969
tdavis37 <tdavis37@5111S-09>
2017-04-12 11:34:23 -0500

    original

--- FirstService/FirstService/Program.cs
+++ FirstService/FirstService/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ *  Author: Troy Davis
+ *  Project: Module11 - WindowsFormsDesign
+ *      - Software Development Fundamentals Lesson 5
+ *        -- Understanding Windows Services - exercise p132
+ *        
+ *  NOTE: see https://www.stevefenton.co.uk/2015/06/add-visual-studio-command-prompt-to-visual-studio-2015/
+ *      - shows how to add the VS 2015 Cmd Prompt to Tools
+ *        -- unfortunately didn't help since you must "Run As Administrator" and VS2015 doesn't give this option   
+ *      
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Apr 12, 2017 
+ *  Revision: Original
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
